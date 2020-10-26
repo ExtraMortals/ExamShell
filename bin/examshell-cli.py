caeefca37484e4226667f22c7ae9cdaca00c8dc3
Exam 42 <exam-no-reply@42.fr>
2020-10-26 16:30:41 +0900

    so funny

---
+++ bin/examshell-cli.py
@@ -0,0 +1,4 @@
+#!/usr/share/examshell/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'examshell==2.0','examshell-cli.py'
+__requires__ = 'examshell==2.0'
+__import__('pkg_resources').run_script('examshell==2.0', 'examshell-cli.py')
---
+++ bin/irl_checker.py
@@ -0,0 +1,4 @@
+#!/usr/share/examshell/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'examshell==2.0','irl_checker.py'
+__requires__ = 'examshell==2.0'
+__import__('pkg_resources').run_script('examshell==2.0', 'irl_checker.py')
